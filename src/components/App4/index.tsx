fff9552ff9df3fac7637548d2636f23e0c17c140
edgar <barrosonetojose@gmail.com>
2023-10-04 13:36:08 -0300

    update accessToken

--- src/components/App4/index.tsx
+++ src/components/App4/index.tsx
@@ -2,11 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { FeatureGroup, LayersControl, TileLayer } from "react-leaflet";
 import { MapContainerStyle } from "./style";
 import { GeoJSONInterface } from "./../../../src/@types/style";
-import {
-  findCenter,
-  MapStyles,
-  convertKmlToGeojson,
-} from "../../utils";
+import { findCenter, MapStyles, convertKmlToGeojson } from "../../utils";
 import { ButtonBar } from "../ButtonBar";
 import { PostesGeojson } from "./components/ElementsGeojson/PostesGeojson";
 import { CtosGeojson } from "./components/ElementsGeojson/CtosGeojson";
@@ -14,87 +10,87 @@ import { CabosGeojson } from "./components/ElementsGeojson/CabosGeojson";
 import { ButtonHome } from "../ButtonHome";
 import { ButtonChangeMap } from "../ButtonChangeMap";
 
-
 interface props {
-  fileContent: string;
+    fileContent: string;
 }
 
-
 const initialGeoJSON: GeoJSONInterface = {
-  type: "FeatureCollection",
-  features: [],
+    type: "FeatureCollection",
+    features: [],
 };
 
 export function App4({ fileContent }: props) {
-  const refTileLayer = useRef(null);
-  const mapStyles = useMemo(() => new MapStyles(), []);
-  const [geojson, setGeojson] = useState(convertKmlToGeojson(fileContent));
-  const [geojsonPostes, setGeojsonPostes] =
-    useState<GeoJSONInterface>(initialGeoJSON);
-  const [geojsonCabos, setGeojsonCabos] =
-    useState<GeoJSONInterface>(initialGeoJSON);
-  const [geojsonCtos, setGeojsonCtos] =
-    useState<GeoJSONInterface>(initialGeoJSON);
-  useEffect(() => {
-    setGeojsonPostes((state) => ({
-      ...state,
-      features: geojson.features.filter(
-        (feature) =>
-          feature.properties.folderName === "Postes" &&
-          feature.geometry.type === "Point"
-      ),
-    }));
-    setGeojsonCtos((state) => ({
-      ...state,
-      features: geojson.features.filter(
-        (feature) =>
-          feature.properties.folderName === "Caixas" &&
-          feature.geometry.type === "Point"
-      ),
-    }));
+    const refTileLayer = useRef(null);
+    const mapStyles = useMemo(() => new MapStyles(), []);
+    const [geojson, setGeojson] = useState(convertKmlToGeojson(fileContent));
+    const [geojsonPostes, setGeojsonPostes] =
+        useState<GeoJSONInterface>(initialGeoJSON);
+    const [geojsonCabos, setGeojsonCabos] =
+        useState<GeoJSONInterface>(initialGeoJSON);
+    const [geojsonCtos, setGeojsonCtos] =
+        useState<GeoJSONInterface>(initialGeoJSON);
+    useEffect(() => {
+        setGeojsonPostes((state) => ({
+            ...state,
+            features: geojson.features.filter(
+                (feature) =>
+                    ["postes", "poste"].includes(
+                        feature.properties.folderName.toLowerCase()
+                    ) && feature.geometry.type === "Point"
+            ),
+        }));
+        setGeojsonCtos((state) => ({
+            ...state,
+            features: geojson.features.filter(
+                (feature) =>
+                    ["caixas", "ceos", "caixa", "ceo"].includes(
+                        feature.properties.folderName.toLowerCase()
+                    ) && feature.geometry.type === "Point"
+            ),
+        }));
 
-    setGeojsonCabos((state) => ({
-      ...state,
-      features: geojson.features.filter(
-        (feature) =>
-          feature.properties.folderName === "Cabos" &&
-          feature.geometry.type === "LineString"
-      ),
-    }));
+        setGeojsonCabos((state) => ({
+            ...state,
+            features: geojson.features.filter(
+                (feature) =>
+                    ["cabos", "cabo", "rede"].includes(
+                        feature.properties.folderName.toLowerCase()
+                    ) && feature.geometry.type === "LineString"
+            ),
+        }));
+    }, [geojson]);
 
-  },[geojson])
+    const changeMap = () => {
+        mapStyles.changeMap();
+        //@ts-ignore
+        refTileLayer.current!.setUrl(mapStyles.getUrl());
+    };
 
-  const changeMap = () => {
-    mapStyles.changeMap();
-    //@ts-ignore
-    refTileLayer.current!.setUrl(mapStyles.getUrl());
-  };
-
-  return (
-    <MapContainerStyle
-      zoomControl={false}
-      center={findCenter(geojson)}
-      zoom={14}
-      doubleClickZoom={false}
-    >
-      <TileLayer
-        ref={refTileLayer}
-        attribution={mapStyles.getAttribution()}
-        url={mapStyles.getUrl()}
-      />
-      <LayersControl position="bottomleft">
-        <FeatureGroup>
-          <CabosGeojson geojsonCabos={geojsonCabos} />
-          <CtosGeojson geojsonCtos={geojsonCtos} />
-          <PostesGeojson geojsonPostes={geojsonPostes} />
-        </FeatureGroup>
-      </LayersControl>
-      <ButtonBar>
-        <ButtonChangeMap changeMap={changeMap} />
-        {/* <ButtonCreateCto/>
+    return (
+        <MapContainerStyle
+            zoomControl={false}
+            center={findCenter(geojson)}
+            zoom={14}
+            doubleClickZoom={false}
+        >
+            <TileLayer
+                ref={refTileLayer}
+                attribution={mapStyles.getAttribution()}
+                url={mapStyles.getUrl()}
+            />
+            <LayersControl position="bottomleft">
+                <FeatureGroup>
+                    <CabosGeojson geojsonCabos={geojsonCabos} />
+                    <CtosGeojson geojsonCtos={geojsonCtos} />
+                    <PostesGeojson geojsonPostes={geojsonPostes} />
+                </FeatureGroup>
+            </LayersControl>
+            <ButtonBar>
+                <ButtonChangeMap changeMap={changeMap} />
+                {/* <ButtonCreateCto/>
         <ButtonCreateCabo/> */}
-      </ButtonBar>
-      <ButtonHome />
-    </MapContainerStyle>
-  );
+            </ButtonBar>
+            <ButtonHome />
+        </MapContainerStyle>
+    );
 }
